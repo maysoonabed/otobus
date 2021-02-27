77c8583330d148d3c42e247d1d24940add4ba201
Samahtbs <samah.tobasi@gmail.com>
2021-02-27 22:49:26 +0200

    ss

--- lib/screens/LoginPage.dart
+++ lib/screens/LoginPage.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
 import '../main.dart';
+import 'SignupPage.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -7,75 +11,269 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  String name, email, password, phone;
+  String errormsg;
+  bool error, showprogress;
+  TextEditingController _password;
+  TextEditingController _phone;
+  FocusNode _focusNodep1;
+  FocusNode _focusNodep2;
+
+  startLogin() async {
+    String apiurl = "http://10.0.0.15/otobus/regpass.php";
+
+    var response = await http.post(apiurl, body: {
+      'phone': phone,
+      'password': password //get password text
+    });
+
+    if (response.statusCode == 200) {
+      var jsondata = json.decode(response.body);
+      if (jsondata["error"]) {
+        setState(() {
+          showprogress = false; //don't show progress indicator
+          error = true;
+          errormsg = jsondata["message"];
+        });
+      } else {
+        if (jsondata["success"]) {
+          setState(() {
+            error = false;
+            showprogress = false;
+          });
+        } else {
+          showprogress = false; //don't show progress indicator
+          error = true;
+          errormsg = "Something went wrong.";
+        }
+      }
+    } else {
+      setState(() {
+        showprogress = false; //don't show progress indicator
+        error = true;
+        errormsg = "Error during connecting to server.";
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    phone = "";
+    password = "";
+    errormsg = "";
+    error = false;
+    showprogress = false;
+    _focusNodep1 = FocusNode();
+    _focusNodep1.addListener(() {
+      if (_focusNodep1.hasFocus) _phone.clear();
+    });
+    _focusNodep2 = FocusNode();
+    _focusNodep2.addListener(() {
+      if (_focusNodep2.hasFocus) _password.clear();
+    });
+    //_name.text = "defaulttext";
+    //_password.text = "defaultpassword";
+    super.initState();
+  }
+
   Widget build(BuildContext context) {
-    return Material(
-      type: MaterialType.transparency,
-      child: new Container(
-        alignment: Alignment.bottomCenter,
-        color: Colors.white,
-        width: double.infinity,
-        child: ListView(
-          children: [
-            SizedBox(
-              height: 100.0,
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(statusBarColor: Colors.transparent
+            //color set to transperent or set your own color
+            ));
+
+    return Scaffold(
+      body: SingleChildScrollView(
+          child: Container(
+        constraints:
+            BoxConstraints(minHeight: MediaQuery.of(context).size.height
+                //set minimum height equal to 100% of VH
+                ),
+        width: MediaQuery.of(context).size.width,
+        //make width of outer wrapper to 100%
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: myGradients1,
+          ),
+        ), //show linear gradient background of page
+
+        padding: EdgeInsets.all(20),
+        child: Column(children: <Widget>[
+          /*************************************************************/
+          Container(
+            margin: EdgeInsets.only(top: 80),
+            child: Text(
+              "تسجيل الدخول",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                  fontFamily: 'Lemonada', //'ArefRuqaaR',
+                  fontWeight: FontWeight.bold),
+            ), //title text
+          ),
+          /*************************************************************/
+          Container(
+            //show error message here
+            margin: EdgeInsets.only(top: 30),
+            padding: EdgeInsets.all(10),
+            child: error ? errmsg(errormsg) : Container(),
+            //if error == true then show error message
+            //else set empty container as child
+          ),
+          /*************************************************************/
+          Container(
+            padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+            margin: EdgeInsets.only(top: 10),
+            child: TextFormField(
+              controller: _phone, //set username controller
+              focusNode: _focusNodep1,
+              maxLength: 13,
+              style: TextStyle(
+                  color: Colors.white, fontSize: 20, fontFamily: 'Lemonada'),
+              keyboardType: TextInputType.number,
+              decoration: myInputDecoration(
+                label: "رقم الهاتف",
+                icon: Icons.phone_android,
+              ),
+              onChanged: (value) {
+                phone = value;
+              },
             ),
-            Container(
-              alignment: Alignment.center,
-              padding: EdgeInsets.all(10.0),
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(colors: myGradients4),
-                  borderRadius: BorderRadius.circular(20.0)
-                  /*boxShadow: new BoxShadow(
-                    color: Colors.red,
-                      offset: new Offset(20.0, 10.0),
-                   )*/
-                  ),
-              child: TextField(
-                  //controller: _username,
+          ),
+          /*************************************************************/
+          Container(
+            padding: EdgeInsets.all(10),
+            child: TextField(
+              controller: _password, //set password controller
+              focusNode: _focusNodep2,
+              style: TextStyle(
+                  color: Colors.white, fontSize: 20, fontFamily: 'Lemonada'),
+              obscureText: true,
+              decoration: myInputDecoration(
+                label: "كلمة السر",
+                icon: Icons.lock,
+              ),
+              onChanged: (value) {
+                password = value;
+              },
+            ),
+          ),
+          /*************************************************************/
+          Container(
+            padding: EdgeInsets.all(10),
+            margin: EdgeInsets.only(top: 20),
+            child: SizedBox(
+              height: 60,
+              width: double.infinity,
+              child: RaisedButton(
+                onPressed: () {
+                  setState(() {
+                    //show progress indicator on click
+                    showprogress = true;
+                  });
+                  startLogin();
+                },
+                child: showprogress
+                    ? SizedBox(
+                        height: 30,
+                        width: 30,
+                        child: CircularProgressIndicator(
+                          backgroundColor: apcolor,
+                          valueColor: AlwaysStoppedAnimation<Color>(
+                              Colors.lightGreenAccent),
+                        ),
+                      )
+                    : Text(
+                        "تسجيل الدخول",
+                        style: TextStyle(fontSize: 20, fontFamily: 'Lemonada'),
+                      ),
+                // if showprogress == true then show progress indicator
+                // else show "LOGIN NOW" text
+                colorBrightness: Brightness.dark,
+                color: apcolor,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30)
+                    //button corner radius
+                    ),
+              ),
+            ),
+          ),
+          /*************************************************************/
+          Container(
+            padding: EdgeInsets.all(10),
+            margin: EdgeInsets.only(top: 20),
+            child: InkResponse(
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => SignupPage()));
+                },
+                child: Text(
+                  "ليس لديك حساب؟ إنشاء حساب جديد",
                   style: TextStyle(
                       color: Colors.white,
-                      /*fontFamily: ArefRuqaa,*/ fontSize: 20),
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    labelText: "رقم الهاتف",
-                    labelStyle: TextStyle(
-                      color: Colors.white,
-                    ),
-                  )),
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Container(
-              alignment: Alignment.center,
-              padding: EdgeInsets.all(10.0),
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(colors: myGradients4),
-                  borderRadius: BorderRadius.circular(20.0)),
-              child: TextField(
-                  //controller: _password,
-                  style: TextStyle(
-                      color: Colors.white,
-                      /*fontFamily: ArefRuqaa,*/ fontSize: 20),
-                  //textAlign: TextAlign.right,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    fillColor: Colors.white,
-                    border: InputBorder.none,
-                    labelText: ("كلمة السر"),
-                    labelStyle: TextStyle(
-                      color: Colors.white,
-                    ),
-                    alignLabelWithHint: false,
-                  )),
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-          ],
-        ),
-      ),
+                      fontSize: 15,
+                      fontFamily: 'Lemonada'),
+                )),
+          )
+        ]),
+      )),
+    );
+  }
+
+  InputDecoration myInputDecoration({String label, IconData icon}) {
+    return InputDecoration(
+      hintText: label, //show label as placeholder
+      alignLabelWithHint: true,
+      //prefixText: '+97',
+      hintStyle: TextStyle(
+        color: Colors.white.withOpacity(0.4),
+        fontSize: 20,
+      ), //hint text style
+      prefixIcon: Padding(
+          padding: EdgeInsets.only(left: 20, right: 10),
+          child: Icon(
+            icon,
+            color: Colors.white,
+          )
+          //padding and icon for prefix
+          ),
+
+      //contentPadding: EdgeInsets.fromLTRB(111, 15, 0, 15),
+      enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30),
+          borderSide:
+              BorderSide(color: apcolor, width: 1)), //default border of input
+
+      focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(40),
+          borderSide: BorderSide(color: Colors.white, width: 1)),
+
+      //focus border
+      fillColor: apcolor,
+      filled: false, //set true if you want to show input background
+    );
+  }
+
+  Widget errmsg(String text) {
+    //error message widget.
+    return Container(
+      padding: EdgeInsets.all(15.00),
+      margin: EdgeInsets.only(bottom: 10.00),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30),
+          color: Colors.red,
+          border: Border.all(color: Colors.red[300], width: 2)),
+      child: Row(children: <Widget>[
+        Container(
+          margin: EdgeInsets.only(right: 6.00),
+          child: Icon(Icons.info, color: Colors.white),
+        ), // icon for error message
+
+        Text(text, style: TextStyle(color: Colors.white, fontSize: 18)),
+        //show error message text
+      ]),
     );
   }
 }
