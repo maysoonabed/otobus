d6962c235d7b62d2ae645e6c8dff1b9e01fe5f05
Samahtbs <samah.tobasi@gmail.com>
2021-02-27 19:01:00 +0200

    ss

--- lib/screens/LoginPage.dart
+++ lib/screens/LoginPage.dart
@@ -8,24 +8,72 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false, //لإخفاء شريط depug
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "Login",
-            style: TextStyle(
-              fontSize: 25,
-              fontFamily: 'Pacifico',
-              color: Colors.white,
+    return Material(
+      type: MaterialType.transparency,
+      child: new Container(
+        alignment: Alignment.bottomCenter,
+        color: Colors.white,
+        width: double.infinity,
+        child: ListView(
+          children: [
+            SizedBox(
+              height: 100.0,
             ),
-          ),
-          backgroundColor: apcolor,
-        ),
-        body: Container(
-          alignment: Alignment.bottomCenter,
-          color: bacolor,
-          width: double.infinity,
+            Container(
+              alignment: Alignment.center,
+              padding: EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(colors: myGradients4),
+                  borderRadius: BorderRadius.circular(20.0)
+                  /*boxShadow: new BoxShadow(
+                    color: Colors.red,
+                      offset: new Offset(20.0, 10.0),
+                   )*/
+                  ),
+              child: TextField(
+                  //controller: _username,
+                  style: TextStyle(
+                      color: Colors.white,
+                      /*fontFamily: ArefRuqaa,*/ fontSize: 20),
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    labelText: "رقم الهاتف",
+                    labelStyle: TextStyle(
+                      color: Colors.white,
+                    ),
+                  )),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Container(
+              alignment: Alignment.center,
+              padding: EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(colors: myGradients4),
+                  borderRadius: BorderRadius.circular(20.0)),
+              child: TextField(
+                  //controller: _password,
+                  style: TextStyle(
+                      color: Colors.white,
+                      /*fontFamily: ArefRuqaa,*/ fontSize: 20),
+                  //textAlign: TextAlign.right,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    fillColor: Colors.white,
+                    border: InputBorder.none,
+                    labelText: ("كلمة السر"),
+                    labelStyle: TextStyle(
+                      color: Colors.white,
+                    ),
+                    alignLabelWithHint: false,
+                  )),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+          ],
         ),
       ),
     );
