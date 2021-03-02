a895940ec2367700cca1ebc2cd86a91a4a944e59
Samahtbs <samah.tobasi@gmail.com>
2021-03-02 15:38:15 +0200

    ss2_3

--- login.php
+++ login.php
@@ -1,62 +1,35 @@
 <?php
-<<<<<<< HEAD:logpass.php
-    $con = new mysqli("localhost","root","","otobus") or die('No connection');
-=======
-$con = mysqli_connect('localhost', 'root', '', 'otobus') or die('No connection');
-if($_SERVER['REQUEST_METHOD'] == "POST"){
-    $passphone=$_POST["phone"];
-    $password =$_POST["password"];
-    $idtype =$_POST["id"];
+    $connect = mysqli_connect("localhost","root","","otobus");      
+ 
+    $Mobile = $_POST['phone'];
+    $Password = md5($_POST['password']);   
+    $idtype=(int)($_POST['id']);
+    //echo  $Password;
 
-    if($idtype==1){
-      $query = mysqli_query($con,"SELECT * FROM `passenger` WHERE `phonenum`='$passphone' AND `password`='$password'");
-      $cek = mysqli_fetch_array($query);
-     
-    if(isset($cek) && $cek != null){
-        echo json_encode("success");
-    }else{
-        echo json_encode("fail");
-    }
-   }
-   else{}
-   }
-<<<<<<< HEAD
-/*
-    $con = mysqli_connect('localhost', 'root', '', 'otobus') or die('No connection');
->>>>>>> b1052d5ecfc829ca0d9759a2cd2e39095211e358:login.php
-    $passphone=$_POST["phone"];
-    $password =$_POST["password"];
-    $idtype =$_POST["id"];
-
-    if($idtype==1)
-   $query = "SELECT * FROM passenger WHERE ( phonenum='$passphone') AND password = '$Password' ";
-   else 
-        $query = "SELECT * FROM `driver` WHERE `phonenum`='$passphone' AND `password`='$password'";
-   $result = mysqli_query($con, $query);
-    
-     
-     if ($result->num_rows > 0)
-        echo json_encode('success');//"passenger is exist"; //$Status->success = "YES";
-     else
-        echo json_encode('fail'); //"passenger is not exist"//$Status->success = "NO";
-<<<<<<< HEAD:logpass.php
-  
-=======
-    }else //****Driver***********************/
-    {
-        $query = "SELECT * FROM `driver` WHERE `phonenum`='$passphone' AND `password`='$password'";
-        $result = $startConn->query($query);
-        if ($result->num_rows > 0)
-           echo json_encode($Status);//"passenger is exist"; //$Status->success = "YES";
-        else
-           echo json_encode($Status);
-  }
-  
-    
->>>>>>> b1052d5ecfc829ca0d9759a2cd2e39095211e358:login.php
-=======
->>>>>>> 972db1bdb4b8d3a5a712e5b1cb73fdbc00ee561f
-
+    if ($connect) {
+       $json['error'] =0;
+       if($idtype==2){     
+          $query = "SELECT * FROM `passenger` WHERE `phonenum`='$Mobile' AND `password` ='$Password'";
+       }else{
+       $query = "SELECT * FROM `driver` WHERE `phonenum`='$Mobile' AND `password` ='$Password'"; 
+       }
+       //echo $query;
+      $result = $connect->query($query);
+      //$result = mysqli_query($connect, $query);
+      //echo $result->num_rows;
+      if($result->num_rows>0){
+         $json['value'] = 1;
+      }else{
+         $json['value'] = 2;
+         $json['message'] = 'رقم الهاتف أو كلمة المرور غير صحيحة';
+         $json['error'] =1;
+      }
+   } else {
+      $json['error'] =1;
+      $json['message'] = 'هناك مشكلة في الاتصال بالسيرفر';
+  }  
+   echo json_encode($json);
+   mysqli_close($connect);  
 ?>
 
 
