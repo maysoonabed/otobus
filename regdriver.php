3f5db6c00afe82050e74af6791b5219d6d6b0248
Samahtbs <samah.tobasi@gmail.com>
2021-03-03 14:47:48 +0200

    ss

--- regdriver.php
+++ regdriver.php
@@ -1,7 +1,7 @@
 <?php 
     $connect = mysqli_connect("localhost","root","","otobus"); 
 	if(!$connect){
-		echo"DAtabase connection failed";		
+		echo"Database connection failed";		
 	}
 	   $card=$_FILES['image']['name'];
 	   $type=$_POST['name'];
@@ -9,7 +9,5 @@
 	   $tmp=$_FILES['image']['tmp'];
 	   move_uploaded_file($tmp,$path);
 	   $connect->query("INSERT INTO bus (type, busid,numofpass,idcard) VALUES ('".$type."','".$type."','".$type."','".$card."')");
-
-
-
+	  
 ?>
