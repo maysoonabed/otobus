2f5a933e7692a9490d2110f8b899d7ed22e0f0bf
Samahtbs <samah.tobasi@gmail.com>
2021-04-01 12:38:13 +0300

    ss

--- lib/screens/DriverMap.dart
+++ lib/screens/DriverMap.dart
@@ -1,14 +1,183 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:OtoBus/dataProvider/address.dart';
+import 'package:OtoBus/dataProvider/appData.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mapbox_autocomplete/flutter_mapbox_autocomplete.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:http/http.dart';
+import 'package:provider/provider.dart';
+import "package:latlong/latlong.dart" as latLng;
 import '../main.dart';
 
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+final List<latLng.LatLng> points = [
+  /* 
+      currLatLng,
+      latLng.LatLng(destinationAdd.lat, destinationAdd.long)
+     */
+];
+final List<Polyline> polyLines = [];
+final List<Marker> markers = [];
+var data;
+latLng.LatLng currLatLng;
+
+const keyPoStack = 'b302ddec67beb4a453f6a3b36393cdf0';
+const keyOpS = 'e29278e269d34185897708d17cb83bc4';
+const tokenkey =
+    'pk.eyJ1IjoibW15eHQiLCJhIjoiY2ttbDMwZzJuMTcxdDJwazVoYjFmN29vZiJ9.zXZhziLKRg0-JEtO4KPG1w';
+final _startPointController = TextEditingController();
+Adress destinationAdd = new Adress();
+
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 class DriverMap extends StatefulWidget {
   @override
   _DriverMapState createState() => _DriverMapState();
 }
 
 class _DriverMapState extends State<DriverMap> {
+  //double mapBottomPadding = 0;
+  Completer<GoogleMapController> _controllerGoogleMap = Completer();
+  GoogleMapController newGoogleMapController;
+  var geoLocator = Geolocator();
+  Position currentPosition;
+  var src_loc = TextEditingController();
+  var des_loc = TextEditingController();
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(31.947351, 35.227163), //Tammun 32.2859658,35.3763749
+    zoom: 14.4746,
+  );
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  void getData(double lat, double long) async {
+    Response response = await get(
+        'http://api.positionstack.com/v1/reverse?access_key=$keyPoStack&query=$lat,$long');
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      setState(() {
+        Adress pickUp = new Adress();
+        pickUp.placeLabel = jsonDecode(data)['data'][0]['label'];
+        pickUp.placeName = jsonDecode(data)['data'][0]['county'];
+        pickUp.long = long;
+        pickUp.lat = lat;
+        src_loc.text = pickUp.placeLabel;
+        setState(() {
+          currLatLng = latLng.LatLng(lat, long);
+        });
+        Provider.of<AppData>(context, listen: false).updatePickAddress(pickUp);
+      });
+    } else {
+      print(response.statusCode);
+    }
+  }
+
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  void setupPositionLocator() async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.bestForNavigation);
+    currentPosition = position;
+
+    LatLng pos = LatLng(currentPosition.latitude, currentPosition.longitude);
+    /* print(currentPosition.latitude);print(currentPosition.longitude); */
+    CameraPosition cp = new CameraPosition(target: pos, zoom: 14);
+    newGoogleMapController.animateCamera(CameraUpdate.newCameraPosition(cp));
+    getData(currentPosition.latitude, currentPosition.longitude);
+  }
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+/* 
+  void getDirDetails() async {
+    String url_try =
+        "https://maps.googleapis.com/maps/api/directions/json?origin=32.2859658,35.3763749&destination=31.947351,%2035.227163&key=AIzaSyDpIlaxbh4WTp4_Ecnz4lupswaRqyNcTv4";
+     
+    String url =
+        "https://maps.googleapis.com/maps/api/directions/json?origin=${currentPosition.latitude},${currentPosition.longitude}&destination=${destinationAdd.lat},${destinationAdd.long}&key=AIzaSyDpIlaxbh4WTp4_Ecnz4lupswaRqyNcTv4";
+    print(url);
+  } */
+
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  Future<void> _searchDialog() async {
+    return showDialog<void>(
+      builder: (context) => new AlertDialog(
+        contentPadding: EdgeInsets.all(20.0),
+        content: Container(
+            width: 300.0,
+            height: 200.0,
+            child: Column(
+              children: <Widget>[
+                new Expanded(
+                  child: new TextField(
+                    controller: src_loc,
+                    readOnly: true,
+                    minLines: 1,
+                    maxLines: null,
+                    autofocus: false,
+                    decoration:
+                        new InputDecoration(labelText: 'Source Location'),
+                  ),
+                ),
+                new Expanded(
+                  child: CustomTextField(
+                    hintText: "Select starting point",
+                    textController: _startPointController,
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => MapBoxAutoCompleteWidget(
+                            apiKey: tokenkey,
+                            hint: "Select starting point",
+                            closeOnSelect: true,
+                            onSelect: (place) {
+                              _startPointController.text = place.placeName;
+                              setState(() {
+                                destinationAdd.lat = place.center[1];
+                                destinationAdd.long = place.center[0];
+                                destinationAdd.placeName = place.placeName;
+                                //***********************
+                                LatLng posd = LatLng(
+                                    destinationAdd.lat, destinationAdd.long);
+                                CameraPosition cpd =
+                                    new CameraPosition(target: posd, zoom: 14);
+                                newGoogleMapController.animateCamera(
+                                    CameraUpdate.newCameraPosition(cpd));
+                                //***********************
+                              });
+                            },
+                            limit: 30,
+                            country: 'Ps',
+                            //language: 'ar',
+                          ),
+                        ),
+                      );
+                    },
+                    enabled: true,
+                  ),
+                )
+              ],
+            )),
+        actions: <Widget>[
+          new FlatButton(
+              child: const Text('CANCEL'),
+              onPressed: () {
+                Navigator.pop(context);
+              }),
+          new FlatButton(
+              child: const Text('CHOOSE'),
+              onPressed: () {
+                setState(() {
+                  //getDirDetails();
+                });
+                Navigator.pop(context);
+              })
+        ],
+      ),
+      context: context,
+    );
+  }
+
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
     return MaterialApp(
@@ -29,7 +198,7 @@ class _DriverMapState extends State<DriverMap> {
           ),
           body: Stack(children: [
             GoogleMap(
-              padding: EdgeInsets.only(bottom: mapBottomPadding),
+              //padding: EdgeInsets.only(bottom: mapBottomPadding),
               mapType: MapType.normal,
               myLocationEnabled: true,
               zoomGesturesEnabled: true,
@@ -39,22 +208,24 @@ class _DriverMapState extends State<DriverMap> {
               onMapCreated: (GoogleMapController controller) {
                 _controllerGoogleMap.complete(controller);
                 newGoogleMapController = controller;
-                setState(() {
+                /*  setState(() {
                   mapBottomPadding = 65;
-                });
+                }); */
                 setupPositionLocator();
               },
             ),
           ]),
           bottomNavigationBar: CurvedNavigationBar(
             color: apcolor,
-            backgroundColor: Colors.white,
+            backgroundColor: ba1color,
             items: <Widget>[
+              // IconButton(icon:
               Icon(
                 Icons.messenger,
                 size: 25,
                 color: Colors.white,
               ),
+              // onPressed: (){ _searchDialog();},
               Icon(
                 Icons.notifications,
                 size: 25,
