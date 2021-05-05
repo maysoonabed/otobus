c28fdb26919808f7ec24a30bd7a4a2f9f686fc68
Samahtbs <samah.tobasi@gmail.com>
2021-05-06 00:16:02 +0300

    ss

--- lib/screens/LoginPage.dart
+++ lib/screens/LoginPage.dart
@@ -49,7 +49,7 @@ class _LoginPageState extends State<LoginPage> {
   startLogin() async {
     SystemChannels.textInput.invokeMethod('TextInput.hide');
     String apiurl =
-        "http://10.0.0.9/otobus/phpfiles/login.php"; //10.0.0.9////192.168.1.108:8089
+        "http://192.168.1.108:8089/otobus/phpfiles/login.php"; //10.0.0.9////192.168.1.108:8089
 
     var response = await http.post(apiurl, body: {
       'email': email,
@@ -93,7 +93,7 @@ class _LoginPageState extends State<LoginPage> {
             Navigator.push(
                 context,
                 MaterialPageRoute(
-                    builder: (context) => PassengerPage())); //PassengerPage()//PassMap
+                    builder: (context) => PassMap())); //PassengerPage()//
           } else {
             await FlutterSession().set('driveremail', email);
 
